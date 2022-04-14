34cb24580a9a046c4eafd25327c0e734f314ca61
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-14 11:58:32 +0100

    data types

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
 console.log("hey typescript i am new here")
+
+
+
+const typesScript=(num1:number, num2:number,boo:boolean, str:string)=>{
+     const result=num1+num2
+   if(boo){
+     return str+result
+   }
+}
+
+const num1: number=12
+const num2 :number=3
+const boo:boolean=true
+const str:string="hey check your result"
+
+typesScript(num1, num2,boo,str)
