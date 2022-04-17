f2467eef487e58664c8e920f031c2e89e3acce46
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-17 13:02:02 +0100

    types

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("hey typescript i am new here");
+// console.log("hey typescript i am new here")
+// const typesScript=(num1:number, num2:number,boo:boolean, str:string)=>{
+//      const result=num1+num2
+//    if(boo)
+//        str+result
+//     return result   
+// }
+// const num1: number=12
+// const num2 :number=3
+// const boo:boolean=true
+// const str:string="hey check your result"
+// typesScript(num1, num2,boo,str)
+// const person:{
+//   name:string,
+//   email:string,
+//   phone:number,
+//   role:[number,string]
+// }={
+//   name:"ayomiku olatunji",
+//   email:"ayomiku@gmail.com",
+//   phone:09025252652,
+//   role:[12,"john"]
+// }
+// person.role.push("bayo")
+// console.log(person)
+// for(let i of Object.keys(person)) console.log(person[i])
+var con = function (num1, num2) {
+    var result = num1 + num2;
+    return result;
+};
+console.log(con(30, 56));
