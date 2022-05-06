44405dc4ce991c2f1d7682d0614399a952a8e824
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-06 20:10:07 +0100

    template

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,18 @@ class projectInput {
         this.hostElement = document.querySelector("#app");
         const importNode = document.importNode(this.templateElement.content, true);
         this.element = importNode.firstElementChild;
+        this.element.id = "user_input";
         //  input element
+        this.titleInputElement = document.querySelector("#title");
+        this.descInputElement = document.querySelector("#description");
+        this.poepleInputElement = document.querySelector("people");
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,23 @@ class projectInput {
 
         const importNode=document.importNode(this.templateElement.content,true)
         this.element=importNode.firstElementChild as HTMLFormElement
+        this.element.id="user_input"
         //  input element
+        this.titleInputElement=document.querySelector("#title")! 
+        this.descInputElement=document.querySelector("#description")!
+        this.poepleInputElement=document.querySelector("people")!
+
+
+        this.configure()
+
+
         this.attach()
+    }
+    private submitHandler(event:Event){
+
+    }
+    private configure(){
+        this.element.addEventListener("submit", this.submitHandler)
     }
 
     private attach(){
